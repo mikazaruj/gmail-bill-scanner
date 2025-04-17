57509f7114ad2d3f21e9be4c38a7576aafeb7c49
mikazaruj <jessica.hlideas@gmail.com>
2025-04-17 14:39:25 +0200

    Settings.tsx refactor into smaller components, fixed typescript errors, works.

---
+++ src/popup/components/settings/index.ts
@@ -0,0 +1,8 @@
+// Export all settings components from a single file
+export { default as SettingsContainer } from './SettingsContainer';
+export { default as ConnectedServicesSection } from './ConnectedServicesSection';
+export { default as TrustedSourcesSection } from './TrustedSourcesSection';
+export { default as ProcessingOptionsSection } from './ProcessingOptionsSection';
+export { default as ScheduleSection } from './ScheduleSection';
+export { default as ManualScanSection } from './ManualScanSection';
+export { default as FieldMappingSection } from './FieldMappingSection'; 
